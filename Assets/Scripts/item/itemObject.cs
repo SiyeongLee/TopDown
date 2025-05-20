af83a00725c4b7ae9bdc784cabb831ff1b0d5006
SiyoengLee <happyamong123@naver.com>
2025-05-20 16:12:55 +0900

    [Feet]ItemSO로 관리되는 5종의 아이템 생성 완료[11강 과제 완료]

---
+++ Assets/Scripts/item/itemObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemObject : MonoBehaviour
+{
+    [SerializeField] ItemSo data;
+
+    public int GetPoint()
+    {
+        return data.point;
+    }
+}
+
